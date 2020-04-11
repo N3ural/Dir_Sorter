89f6178120d769957183eb9360b9fae2a6634ecb
N3ural <52070747+N3ural@users.noreply.github.com>
2020-04-11 20:50:15 +0200

    Add files via upload

--- Dir_Sorter.py
+++ Dir_Sorter.py
@@ -15,19 +15,19 @@ categories = {
 
 
 def create_directories():
-    folders_to_be_created = set(categories.keys())
-    for folder in folders_to_be_created:
+    for folder in set(categories.keys()):
         Path(directory + "/" + folder).mkdir(exist_ok=True)
 
 
 def organizer(path):
     create_directories()
-    files = [x for x in Path(path).iterdir() if x.is_file()]
-    for src_path in files:
+    for src_path in [x for x in Path(path).iterdir() if x.is_file()]:
         for dirname, extensions in categories.items():
             if extensions is None or src_path.name.endswith(tuple(extensions)):
                 shutil.move(src_path, Path(directory).joinpath(dirname, src_path.name))
                 break
+
+
 
 if __name__ == '__main__':
     organizer(directory)
